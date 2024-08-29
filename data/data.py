d133e5ad109f270eeac3b9d9ac601fdfc681509a
Nicola Ng <78801570+NicolaNg5@users.noreply.github.com>
2024-08-29 20:17:21 +1000

    added in column combinations

--- data/data.py
+++ data/data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import sklearn.preprocessing as prep
 from sklearn.model_selection import train_test_split
+from datetime import datetime 
 
 
 def process_data(data, lags):
@@ -10,15 +11,20 @@ def process_data(data, lags):
     # Get unique scats
     unique_scats = df['SCATS Number'].unique()
 
-    print(unique_scats, len(unique_scats))
+    # print(unique_scats, len(unique_scats))
 
     scat = unique_scats[0]
 
     # Filter the data for that scat number
     scat_data = df[df['SCATS Number'] == scat]
 
+    # Get the date and time columns
+
+    date = scat_data['Date']
+
     # Get the traffic flow columns
     traffic_flow = scat_data.filter(regex=r'^V\d+$').columns
+
 
     # Reshape the data from wide to long
     scat_data_long = pd.melt(
@@ -26,11 +32,20 @@ def process_data(data, lags):
         id_vars=['Date'],
         value_vars=traffic_flow,
         var_name='Time Period',
-        value_name='Traffic Flow'
+        value_name='Lane 1 Flow (Veh/15 Minutes)'
     )
 
-    scat_data_long.to_csv('data/scat_data_long.csv', index=False)
-    print(scat_data_long.head())
+    # Take Time Period interval and change to numerical value return as str
+    scat_data_long['Time'] = ((scat_data_long['Time Period'].str.replace("V", "").astype(int) + 1)).astype(str)
+
+    # Create new 15 Minutes column and combine data and time
+    scat_data_long['15 Minutes'] = scat_data_long['Date'] + " " + scat_data_long['Time']
+
+    # restructure df to only have 5 Minutes and Lane 1 Flow (Veh/5 Minutes)
+    scat_data_long = scat_data_long[['15 Minutes', 'Lane 1 Flow (Veh/15 Minutes)']]
+
+    scat_data_long.to_csv('scat_data_970.csv', index=False)
+    # print(scat_data_long.head())
 
     # # For each scat
     # for scat in unique_scats:
@@ -50,6 +65,6 @@ def process_data(data, lags):
 
 
 if __name__ == '__main__':
-    data = 'data/scats_data.csv'
+    data = 'scats_data.csv'
     lags = 5
     process_data(data, lags)
