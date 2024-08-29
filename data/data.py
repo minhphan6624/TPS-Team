3d388d127588e7b747cfbc5c7a2d6bd20fc5c3e8
minhphan6624 <quangminh6624@gmail.com>
2024-08-29 15:48:22 +1000

    uwu

--- data/data.py
+++ data/data.py
@@ -7,7 +7,43 @@ from sklearn.model_selection import train_test_split
 def process_data(data, lags):
     df = pd.read_csv(data)
 
-    print(df.head())
+    # Get unique scats
+    unique_scats = df['SCATS Number'].unique()
+
+    print(unique_scats, len(unique_scats))
+
+    scat = unique_scats[0]
+
+    # Filter the data for that scat number
+    scat_data = df[df['SCATS Number'] == scat]
+
+    # Get the traffic flow columns
+    traffic_flow = scat_data.filter(regex=r'^V\d+$').columns
+
+    # Reshape the data from wide to long
+    scat_data = pd.melt(
+        scat_data,
+        id_vars=['Date'],
+        value_vars=traffic_flow
+    )
+
+    print(scat_data)
+
+    # # For each scat
+    # for scat in unique_scats:
+
+    #     # Filter the data for that scat number
+    #     scat_data = df[df['SCATS Number'] == scat]
+
+    #     # Get the traffic flow columns
+    #     traffic_flow = scat_data.filter(like='V').columns
+
+    #     # Reshape the data from wide to long
+    #     scat_data = pd.melt(
+    #         scat_data,
+    #         id_vars=['Date', 'Time'],
+    #         value_vars=traffic_flow
+    #     )
 
 
 if __name__ == '__main__':
