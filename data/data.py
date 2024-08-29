b0424c32bb37e5a59ab24c420201cbeac8d84691
minhphan6624 <quangminh6624@gmail.com>
2024-08-29 15:47:41 +1000

    reshaped dataset

--- data/data.py
+++ data/data.py
@@ -5,4 +5,12 @@ from sklearn.model_selection import train_test_split
 
 
 def process_data(data, lags):
-    pass
+    df = pd.read_csv(data)
+
+    print(df.head())
+
+
+if __name__ == '__main__':
+    data = 'data/scats_data.csv'
+    lags = 5
+    process_data(data, lags)
