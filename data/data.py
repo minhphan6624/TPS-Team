02327296afc9a766ad29881983dfad882652167d
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-09-11 16:06:44 +1000

    work on data processing

--- data/data.py
+++ data/data.py
@@ -11,20 +11,12 @@ def process_data(data, lags):
     # Get unique scats
     unique_scats = df['SCATS Number'].unique()
 
-    # print(unique_scats, len(unique_scats))
-
+    # Test with first scat number: 0970
     scat = unique_scats[0]
-
-    # Filter the data for that scat number
     scat_data = df[df['SCATS Number'] == scat]
-
-    # Get the date and time columns
-
-    date = scat_data['Date']
 
     # Get the traffic flow columns
     traffic_flow = scat_data.filter(regex=r'^V\d+$').columns
-
 
     # Reshape the data from wide to long
     scat_data_long = pd.melt(
@@ -38,6 +30,9 @@ def process_data(data, lags):
     # Take Time Period interval and change to numerical value return as str
     scat_data_long['Time'] = ((scat_data_long['Time Period'].str.replace("V", "").astype(int) + 1)).astype(str)
 
+     # Create a mapping of V00, V01, ..., V95 to the actual time intervals
+    time_mapping = {f'V{i:02d}': f'{i//4:02d}:{i%4*15:02d}' for i in range(96)}
+
     # Create new 15 Minutes column and combine data and time
     scat_data_long['15 Minutes'] = scat_data_long['Date'] + " " + scat_data_long['Time']
 
@@ -45,24 +40,7 @@ def process_data(data, lags):
     scat_data_long = scat_data_long[['15 Minutes', 'Lane 1 Flow (Veh/15 Minutes)']]
 
     scat_data_long.to_csv('scat_data_970.csv', index=False)
-    # print(scat_data_long.head())
-
-    # # For each scat
-    # for scat in unique_scats:
-
-    #     # Filter the data for that scat number
-    #     scat_data = df[df['SCATS Number'] == scat]
-
-    #     # Get the traffic flow columns
-    #     traffic_flow = scat_data.filter(like='V').columns
-
-    #     # Reshape the data from wide to long
-    #     scat_data = pd.melt(
-    #         scat_data,
-    #         id_vars=['Date', 'Time'],
-    #         value_vars=traffic_flow
-    #     )
-
+    
 
 if __name__ == '__main__':
     data = 'scats_data.csv'
