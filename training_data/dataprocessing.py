cf4280cf97aebbb7d8635c4ababf1764f57945be
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2024-10-27 18:52:17 +1100

    formatting

--- training_data/dataprocessing.py
+++ training_data/dataprocessing.py
@@ -38,19 +38,17 @@ def fix_data(long):
 
         # Loop through all time intervals and add to processed data
         for i in range(0, 96):
-            # pad the interval number with 0's
+            
+            # Pad the interval number with 0's
             i_padded = str(i).zfill(2)
             column_fix = f"V{i_padded}"
 
-            #print(column_fix)
-
+            # Get the data for the current interval
             interval_data = date_data[column_fix]
-            
-            # print(int(interval_data))
+
 
             # Turn I into a time format like 00:00 multiple by 15 minutes
             # 00:00 = 0
-
             i_time = i * 15
             i_time_formatted = f"{i_time // 60}:{i_time % 60}"
 
@@ -64,7 +62,6 @@ def fix_data(long):
             mins = f"{date} {i_time_formatted}"
 
             if len(interval_data) == 0:
-                #processed_data = processed_data.append({'Date': date, 'Time Interval': i, 'Total': 0}, ignore_index=True)
                 print("Found no data.")
             else:
                 processed_data = processed_data._append({'15 Minutes': mins, 'Lane 1 Flow (Veh/15 Minutes)': int(interval_data.iloc[0])}, ignore_index=True)
@@ -72,55 +69,6 @@ def fix_data(long):
     # Save processed data to csv
     processed_data.to_csv(f'traffic_flows/{scats_number}_{location_split[1]}_trafficflow.csv', index=False)
 
-def process_data(data, lags):
-    global df
-
-    df = pd.read_csv(data)
-
-    # Get unique SCATS numbers
-    unique_df = df['NB_LONGITUDE'].unique()
-    unique_scats = df['SCATS Number'].unique()
-
-    print(f"Unique Df Numbers: {unique_df}, Count: {len(unique_df)}")
-
-    done = 0
-
-    for number in unique_df:
-        print(f"Finished {done} out of {len(unique_df) - 1}")
-        fix_data(number)
-
-        done += 1
-
-    return unique_scats
-
-def merge_datasets():
-    scat_site = '970'
-
-    unique_scats = df['SCATS Number'].unique()
-
-    file_directions = ['E', 'N', 'S', 'W']  
-    base_path = 'traffic_flows'  
-
-    #Initialize the dataframes
-    dataframes = []
-
-    for direction in file_directions:
-        file_name = f"{scat_site}_{direction}_trafficflow.csv"
-        file_path = f"{base_path}/{file_name}"
-
-        #Read the file and add the direction column
-        data = pd.read_csv(file_path)
-        data["directions"] = direction
-
-        #Append the dataframe to the list
-        dataframes.append(data)
-
-    #Concatenate the dataframes
-    final_data = pd.concat(dataframes)
-
-    final_data.to_csv(f"new_traffic_flows/{scat_site}_trafficflow.csv", index=False)  
-
-    print(f"Merged files for SCAT site {scat_site}")
     
 def merge_all_datasets():
     base_path = 'traffic_flows'  
@@ -162,9 +110,29 @@ def merge_all_datasets():
         
         print(f"Merged file saved as {output_file}")
 
+def process_data(data, lags):
+    global df
+
+    df = pd.read_csv(data)
+
+    # Get unique SCATS numbers
+    unique_df = df['NB_LONGITUDE'].unique()
+    unique_scats = df['SCATS Number'].unique()
+
+    print(f"Unique Df Numbers: {unique_df}, Count: {len(unique_df)}")
+
+    done = 0
+
+    for number in unique_df:
+        print(f"Finished {done} out of {len(unique_df) - 1}")
+        fix_data(number)
+
+        done += 1
+
+    return unique_scats
 
 if __name__ == '__main__':
     data = 'scats_data.csv'
     lags = 5
-    # process_data(data, lags)
+    process_data(data, lags)
     merge_all_datasets()
